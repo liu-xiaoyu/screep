0d84d1ed525386721eebfe265b8f3257fc8aaf1a
liu-xiaoyu <liuxiaoyuuu@gmail.com>
2017-07-04 13:51:53 -0400

    first commit

---
+++ construct.js
@@ -0,0 +1,26 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('population');
+ * mod.thing == 'a thing'; // true
+ */
+var construct = {
+    extension: function(flag, structure_type){
+        let x = flag.pos['x'];
+        let y = flag.pos['y'];
+        flag.room.createConstructionSite(x-2,y,structure_type);
+        flag.room.createConstructionSite(x-1,y,structure_type);
+        flag.room.createConstructionSite(x,y,structure_type);
+        flag.room.createConstructionSite(x+1,y,structure_type);
+        flag.room.createConstructionSite(x+2,y,structure_type);
+    },
+    road: function(source, target){
+        let path = source.pos.findPathTo(target.pos);
+        for (let dot of path){
+            source.room.createConstructionSite(dot['x'],dot['y'],STRUCTURE_ROAD);
+        }
+    }
+}
+module.exports = construct;
